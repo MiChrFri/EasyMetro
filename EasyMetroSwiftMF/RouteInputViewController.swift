f113bdfa94ad5963056824602012961f4633c425
Michael <michael.christian.frick@gmail.com>
2017-03-19 11:18:04 +0100

    initial commit

---
+++ EasyMetroSwiftMF/RouteInputViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RouteInputViewController.swift
+//  EasyMetroSwiftMF
+//
+//  Created by Michael Frick on 19/03/2017.
+//  Copyright © 2017 frickm. All rights reserved.
+//
+
+import UIKit
+
+class RouteInputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
