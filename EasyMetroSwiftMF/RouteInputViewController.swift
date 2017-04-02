90c0ac754aab996bc828b1d21ccef5d46ff1c50c
Michael <michael.christian.frick@gmail.com>
2017-04-02 22:46:59 +0200

    improved ux

--- EasyMetroSwiftMF/RouteInputViewController.swift
+++ EasyMetroSwiftMF/RouteInputViewController.swift
@@ -15,21 +15,17 @@ enum direction {
 class RouteInputViewController: UIViewController {
     @IBOutlet weak var departurePicker: UIPickerView!
     @IBOutlet weak var destinationPicker: UIPickerView!
-
     @IBOutlet weak var fromLabel: UILabel!
-    
     @IBOutlet weak var toLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
-    
     @IBOutlet weak var directionsTextView: UITextView!
+    @IBOutlet weak var directionTextView: UITextView!
     
-    
-    let allStations = Array(MarsCityMetroLine.allStations)
+    let allStations = ["------"] + Array(MarsCityMetroLine.allStations)
     var metro = MarsCityMetro()
     let routeFinder = RouteFinder()
     
-    @IBOutlet weak var directionTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -39,21 +35,28 @@ class RouteInputViewController: UIViewController {
     
     @IBAction func searchButtonTapped(_ sender: UIButton) {
         guard let from = fromLabel.text, let to = toLabel.text else { return }
-        
-        if var fromStation = metro.metroGraph.stations[from], let toStation = metro.metroGraph.stations[to] {
-        
-            let shortestPath = routeFinder.breadthFirstSearch(metroGraph: metro.metroGraph, station: &fromStation, destination: toStation)
-            
-            print(shortestPath)
-            
-            let ticketFormatter = TicketFormatter()
-            if let ticketData = ticketFormatter.getTicketData(stations: shortestPath) {
-                durationLabel.text = "Duration: \(ticketData.travelTime) min"
-                priceLabel.text = "Costs: \(ticketData.ticketPrice)$ "
-                directionsTextView.text = ticketData.directions
+        if from == "------" || to == "------" {
+            directionsTextView.text = "Please select a departure and a destination station."
+        } else if from == to {
+            directionsTextView.text = "The destination you picked is the same as the departure station. That might not be what you wanted to select."
+        } else {
+            if var fromStation = metro.metroGraph.stations[from], let toStation = metro.metroGraph.stations[to] {
+                
+                let shortestPath = routeFinder.breadthFirstSearch(metroGraph: metro.metroGraph, station: &fromStation, destination: toStation)
+                
+                let ticketFormatter = TicketFormatter()
+                if let ticketData = ticketFormatter.getTicketData(stations: shortestPath) {
+                    durationLabel.text = "Duration: \(ticketData.travelTime) min"
+                    priceLabel.text = "Costs: \(ticketData.ticketPrice)$ "
+                    directionsTextView.text = ticketData.directions
+                }
             }
+            metro.metroGraph.stations.forEach({ $0.value.visited = false; $0.value.route = [] })
         }
-        metro.metroGraph.stations.forEach({ $0.value.visited = false; $0.value.route = [] })
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 }
 
@@ -71,13 +74,12 @@ extension RouteInputViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return allStations[row]
     }
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
         if pickerView == departurePicker {
             fromLabel.text = allStations[row]
         } else if pickerView == destinationPicker {
             toLabel.text = allStations[row]
         }
-
     }
 }
